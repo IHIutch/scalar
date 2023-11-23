e57afb9b60d5c06cac362504cdb11bbb8fb6dcaa
marclave <marc@scalar.com>
2023-11-23 12:58:22 -0800

    wip use-markdown isolation

--- examples/web/src/pages/StandaloneApiReferencePage.vue
+++ examples/web/src/pages/StandaloneApiReferencePage.vue
@@ -8,7 +8,10 @@ import { reactive } from 'vue'
 const configuration = reactive<ReferenceConfiguration>({
   theme: 'default',
   proxy: 'http://localhost:5051',
-  isEditable: true,
+  isEditable: false,
+  spec: {
+    url: 'https://petstore3.swagger.io/api/v3/openapi.json',
+  },
 
   tabs: {
     initialContent: 'Swagger Editor',
